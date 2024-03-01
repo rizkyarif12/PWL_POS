f5bd331b8786e2d0243b94aa946e133647045c76
rizkyarif12 <116237742+rizkyarif12@users.noreply.github.com>
2024-03-01 17:45:26 +0700

    membuat foreign key antar tabel

--- database/migrations/2024_03_01_023145_create_m_kategori_table.php
+++ database/migrations/2024_03_01_023145_create_m_kategori_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('m_kategori', function (Blueprint $table) {
-            $table->id('level_id');
-            $table->string('kategori_kode', 10)->unique();
+            $table->id('kategori_id');
+            $table->string('kategori_kode',10)->unique();
             $table->string('kategori_nama', 100);
             $table->timestamps();
         });
