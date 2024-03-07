1b91f7212904fa13a5a2071af38534b18a43fc0f
rizkyarif12 <116237742+rizkyarif12@users.noreply.github.com>
2024-03-07 13:10:07 +0700

    Retreiving Aggregrates praktikum 2.3 pertemuan 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -39,7 +39,11 @@ class UserController extends Controller
         // $user = UserModel::all(); // ambil semua data dari tabel m_user
         // return view('user', ['data' => $user]);
 
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
+        // $user = UserModel::where('username', 'manager9')->firstOrFail();
+        // return view('user', ['data' => $user]);
+
+        $user = UserModel::where('level_id', 2)->count();
+        //dd($user);
         return view('user', ['data' => $user]);
     }
 }
