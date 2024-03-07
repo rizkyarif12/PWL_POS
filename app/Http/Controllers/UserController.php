90fc427c85a18efbea0a7cfd42f05345e1836d2e
rizkyarif12 <116237742+rizkyarif12@users.noreply.github.com>
2024-03-07 11:54:04 +0700

    praktikum 2.2 pertemuan 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -39,9 +39,7 @@ class UserController extends Controller
         // $user = UserModel::all(); // ambil semua data dari tabel m_user
         // return view('user', ['data' => $user]);
 
-        $user = UserModel::findOr(20, ['username','nama'], function(){
-            abort(404);
-        });
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
