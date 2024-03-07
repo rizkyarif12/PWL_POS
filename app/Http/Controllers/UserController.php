d39f8faa0561652b2995007bc047b4a2ce671066
rizkyarif12 <116237742+rizkyarif12@users.noreply.github.com>
2024-03-07 16:53:46 +0700

    Relation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,7 +8,9 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index(){
-        $user = UserModel::all();
+        // $user = UserModel::all();
+        // return view('user', ['data' => $user]);
+        $user = UserModel::with('level')->get();
         return view('user', ['data' => $user]);
     }
 
