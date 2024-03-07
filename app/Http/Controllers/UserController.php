8d2435f6ceff3aae54f3d90dd9c944346721235e
rizkyarif12 <116237742+rizkyarif12@users.noreply.github.com>
2024-03-07 11:18:12 +0700

    fillable

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -23,11 +23,18 @@ class UserController extends Controller
         // return view('user', ['data' => $user]);
     
         //tambah data user dengan Eloquent Model
-        $data = [
-            'nama' => 'Pelanggan Pertama',
-        ];
-        UserModel::where('username', 'customer-1')->update($data); // update data user
+        // $data = [
+        //     'nama' => 'Pelanggan Pertama',
+        // ];
+        // UserModel::where('username', 'customer-1')->update($data); // update data user
 
+        $data = [
+            'level_id' =>2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
+        ];
+        UserModel::create($data);
         //coba akses model UserModel
         $user = UserModel::all(); // ambil semua data dari tabel m_user
         return view('user', ['data' => $user]);
