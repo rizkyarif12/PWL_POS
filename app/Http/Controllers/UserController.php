74d4346926e7916d1c7e48897c75d720fa45ed23
rizkyarif12 <116237742+rizkyarif12@users.noreply.github.com>
2024-03-07 11:45:47 +0700

    find praktikum 2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -28,15 +28,20 @@ class UserController extends Controller
         // ];
         // UserModel::where('username', 'customer-1')->update($data); // update data user
 
-        $data = [
-            'level_id' =>2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
-        //coba akses model UserModel
-        $user = UserModel::all(); // ambil semua data dari tabel m_user
+        // $data = [
+        //     'level_id' =>2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345')
+        // ];
+        // UserModel::create($data);
+        // //coba akses model UserModel
+        // $user = UserModel::all(); // ambil semua data dari tabel m_user
+        // return view('user', ['data' => $user]);
+
+        $user = UserModel::findOr(20, ['username','nama'], function(){
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
