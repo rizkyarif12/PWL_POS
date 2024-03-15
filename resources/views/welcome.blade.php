e2f7545a350f97a3645793dbf8f5f65dea7866ae
rizkyarif12 <116237742+rizkyarif12@users.noreply.github.com>
2024-03-15 07:33:48 +0700

    Praktikum 1 pertemuan 5

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -130,4 +130,52 @@
             </div>
         </div>
     </body>
-</html>
+</html> --}}
+
+{{-- @extends('layouts.app') --}}
+
+{{-- Customize layout sections --}}
+
+{{-- @section('subtitle', 'Welcome')
+@section('content_header_title', 'Home')
+@section('content_header_subtitle', 'Welcome') --}}
+
+{{-- Content body: main page content --}}
+
+{{-- @section('content_body')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop --}}
+
+{{-- Push extra CSS --}}
+
+{{-- @push('css') --}}
+{{-- Add here extra stylesheets --}}
+{{-- <link rel-"stylesheet" href-"/css/admin_custom.css"> --}}
+{{-- @endpush --}}
+
+{{-- Push extra scripts --}}
+
+{{-- @push('js')
+<script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
+@endpush --}}
+
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
+
+@section('css')
+    {{-- Add here extra stylesheets --}}
+    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
+@stop
+
+@section('js')
+    <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
+@stop
