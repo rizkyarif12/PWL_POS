2bdf0a5576077ba1a68f4a94b48096e1f3f800b2
rizkyarif12 <116237742+rizkyarif12@users.noreply.github.com>
2024-03-18 09:50:32 +0700

    manage kategori

--- app/Models/KategoriModel.php
+++ app/Models/KategoriModel.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class KategoriModel extends Model
 {
+    protected $table = 'm_kategori';
+    protected $primarykey = 'kategori_id';
+
+    protected $fillable = ['kategori_kode', 'kategori_nama'];
     public function barang(): HasMany{
         return $this->hasMany(BarangModel::class, 'barang_id', 'barang_id');
     }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,9 +2,11 @@
 {{-- Extend and customize the browser title --}}
 
 @section('title')
-    {{ config('admin.title') }}
+    {{ config('adminlte.title') }}
     @hasSection('subtitle') | @yield('subtitle') @endif
 @stop
+
+@vite('resources/js/app.js')
 
 {{-- Extend and customize the page content header --}}
 
@@ -46,17 +48,15 @@
 {{-- Add common Javascript/jquery code --}}
 
 @push('js')
-    <script>
-
-        $(document).ready(function(){
-            //add your common script logic here...
-        });
-    </script>
+<script src="https://cdn.datatables.net/2.0.2/js/dataTables.js"></script>
 @endpush
+
+@stack('scripts')
 
 {{-- Add common CSS customizations --}}
 
 @push('css')
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.0.2/css/dataTables.dataTables.css">
     <style type="text/css">
         /* You can add AdminLTE customizations here */
 
